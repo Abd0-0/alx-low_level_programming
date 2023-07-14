a2dbfc6ccc26a842bd7ccc17ec548c8aa724e470
sherlock <sherlockholmes>
2023-07-14 05:20:59 +0300

    Task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * A c program to using puts function
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
