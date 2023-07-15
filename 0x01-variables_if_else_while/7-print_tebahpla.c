184684007450622d58318c98d4e6abe27efde2e5
Abd0_0 <abdalrahman.magdy2000@gmail.com>
2023-07-14 18:12:58 -0700

    Task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char rev = 'z';
+
+	while (rev >= 'a')
+
+	{
+		putchar(rev);
+		putchar--;
+	}
+
+	putchar('\n');
+	return (0);
+}
