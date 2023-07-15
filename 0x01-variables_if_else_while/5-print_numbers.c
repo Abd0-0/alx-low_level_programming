a79a886bb9b67697558287963d2f4d1487ddd9a0
Abd0_0 <abdalrahman.magdy2000@gmail.com>
2023-07-14 17:31:32 -0700

    Task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+
+	{
+		printf("%i", i);
+	}
+
+	printf("\n");
+	return (0);
+}
