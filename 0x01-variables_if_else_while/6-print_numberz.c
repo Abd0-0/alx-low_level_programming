f06a260d9466d28a1e887352c2cf9dde88e34d02
Abd0_0 <abdalrahman.magdy2000@gmail.com>
2023-07-14 17:38:40 -0700

    Task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+
+	{
+		putchar('i');
+	}
+
+	printf("\n");
+	return (0);
+}
