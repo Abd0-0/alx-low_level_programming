ae36518846648f7260c40cd6380b39bb0b4d0858
Abd0_0 <abdalrahman.magdy2000@gmail.com>
2023-07-18 09:25:56 -0700

    Task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return:0 (Success)
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		putchar(str[i]);
+	}
+
+	putchar('\n');
+}
