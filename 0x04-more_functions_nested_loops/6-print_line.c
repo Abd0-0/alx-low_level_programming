7ce15c1fc14d80dddefc19222efc73bfdfc5e9db
Abd0_0 <abdalrahman.magdy2000@gmail.com>
2023-07-20 16:18:17 -0700

    Task6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,12 +8,7 @@
 
 void print_line(int n)
 {
-	if (n == 0)
-	{
-		_putchar('\n');
-	}
-
-	else
+	if (n > 0)
 	{
 		int i, j;
 
@@ -28,4 +23,10 @@ void print_line(int n)
 			_putchar('\n');
 		}
 	}
+
+	else
+	{
+		_putchar('\n');
+	}
+
 }
