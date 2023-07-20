008c430b058564bbf443638bf2c3fc37ce3e121b
Abd0_0 <abdalrahman.magdy2000@gmail.com>
2023-07-20 14:08:19 -0700

    Tasks(0, 1, 2, 3, 4)

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - a function that checks for uppercase character.
+ *
+ * @c: the ASCII value of the character
+ *
+ * Return: 1 if c is uppercase, else 0.
+*/
+
+int _isupper(int c)
+{
+	if ('A' <= c && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul -  a function that multiplies two integers.
+ *
+ * @a: first number.
+ * @b: 2nd number.
+ *
+ * Return: the result of the proccess
+*/
+
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+	return (sum);
+}
