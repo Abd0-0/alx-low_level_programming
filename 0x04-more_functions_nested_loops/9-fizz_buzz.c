d07aefd88e40d53941422d5568bb7d934bb87469
Abd0_0 <abdalrahman.magdy2000@gmail.com>
2023-07-20 18:09:37 -0700

    Task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,9 +13,9 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0)
+		if ((i % 5) == 0 && (i % 3) == 0)
 		{
-			printf("Fizz ");
+			printf("FizzBuzz ");
 		}
 
 		else if ((i % 5) == 0)
@@ -23,15 +23,19 @@ int main(void)
 			printf("Buzz ");
 		}
 
-		else if (((i % 5) == 0) && ((i % 3) == 0))
+		else if ((i % 3) == 0)
 		{
-			printf("FizzBuzz ");
+			printf("Fizz ");
 		}
 
 		else
 		{
-			printf("%i ", i);
+			printf("%d ", i);
 		}
 	}
+
+	printf("\n");
+
+	return (0);
 
 }
