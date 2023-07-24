f98a23b7425f17176847d45ed0138b58e4b3b485
Abd0_0 <abdalrahman.magdy2000@gmail.com>
2023-07-24 15:17:03 -0700

    Task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string.
+ *
+ * @s: a pointer to a string.
+ *
+ * Return: the length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int len, i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len = i;
+	}
+
+	return (len);
+}
