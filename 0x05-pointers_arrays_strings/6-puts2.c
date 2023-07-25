7742c539a9f3241b81c6ab0b0eb959fdff1a62ea
Abd0_0 <abdalrahman.magdy2000@gmail.com>
2023-07-24 18:37:52 -0700

    Task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,12 +12,14 @@ void puts2(char *str)
 {
 	int i, n;
 
-	for (i = 0; str[i] !=; '0'; i++);
+	for (i = 0; str[i] != '0'; i++);
 
 	n = i - 1;
 
 	for (i = 0; i <= n; i = i + 2)
 	{
-		_putchar[i];
+		_putchar[str[i]];
 	}
+
+	_putchar('\n');
 }
