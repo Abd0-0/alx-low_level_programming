8c14d953ec90f909a6b1dae17e1a8cb82453fbf3
Abd0_0 <abdalrahman.magdy2000@gmail.com>
2023-07-24 19:56:20 -0700

    Task9

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,24 +16,11 @@ void puts_half(char *str)
 
 	n = i;
 
-	if (n % 2 == 0)
+	l = n / 2;
+
+	for (i = l; i <= n - 1; i++)
 	{
-		l = n / 2;
-
-		for (i = l; i <= n - 1; i++)
-		{
-			_putchar(str[i]);
-		}
+		_putchar(str[i]);
 	}
-	else
-	{
-		l = (n - 1) / 2;
-
-		for (i = n - 1 ; i > l; i--)
-		{
-			_putchar(str[i]);
-		}
-	}
-
 	_putchar('\n');
 }
