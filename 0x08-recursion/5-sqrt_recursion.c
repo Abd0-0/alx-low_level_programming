d67cd569be9274cbb2161aaf81033cb408d167a1
abd0-0 <abdaalrahman.mgdy2000@gmail.com>
2023-08-02 18:33:01 -0700

    Project 9, the mandatory tasks

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,11 +10,11 @@
 
 int _sqrt_recursion(int n)
 {
-	return (sqrt(n, 1));
+	return (_sqrt(n, 1));
 }
 
 /**
- * sqrt - unction that returns the natural square root of a n.
+ * _sqrt - unction that returns the natural square root of a n.
  *
  * @n: the number to get it's sqrt.
  * @nn: n * n.
@@ -22,13 +22,13 @@ int _sqrt_recursion(int n)
  * Return: sqrt of n.
  */
 
-int sqrt(int n, int nn)
+int _sqrt(int n, int nn)
 {
 	if (nn * nn == n)
 		return (nn);
 
 	else if (nn * nn < n)
-		return (sqrt(n, nn + 1));
+		return (_sqrt(n, nn + 1));
 
 	else
 		return (-1);
