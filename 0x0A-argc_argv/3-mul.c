a7b02608e60e0253ab9008e566e0657158bb49ef
abd0-0 <abdaalrahman.mgdy2000@gmail.com>
2023-08-04 18:10:24 -0700

    task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,20 +12,20 @@
 
 int main(int argc, char **argv)
 {
-	if (argc != 3 && argc == 1)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	else
+	if (argc == 3)
 	{
 		int f = atoi(argv[1]), s = atoi(argv[2]), sum;
 
 		sum = f * s;
 
 		printf("%d\n", sum);
+		return (0);
 	}
 
-	return (0);
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
 }
