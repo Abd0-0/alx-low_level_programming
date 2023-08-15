d493b58f0c4ec8f8c48a9fd82d1cdfd4751faa3e
abd0-0 <abdaalrahman.mgdy2000@gmail.com>
2023-08-14 17:07:34 -0700

    Task2

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTION_LIKE_MACRO.H
 #define FUNCTION_LIKE_MACRO.H
 
-#define ABS(x) do {((x) < 0) ? (-(x)) : (x)} while (0)
+#define ABS(x) do {(x) < 0 ? (-(x)) : (x)} while (0)
 
 
 #endif /* #ifndef FUNCTION_LIKE_MACRO.H */
