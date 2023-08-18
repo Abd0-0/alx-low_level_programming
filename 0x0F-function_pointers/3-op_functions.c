62a9817be166df7f15711d611e49c17c347fe83c
abd0-0 <abdaalrahman.mgdy2000@gmail.com>
2023-08-18 07:20:07 -0700

    Task3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,97 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two number.
+ *
+ * @a: the 1at number.
+ * @b: the 2nd number.
+ *
+ * Return: the calculation output.
+ *
+ */
+
+int op_add(int a, int b)
+{
+	int z;
+
+	z = a + b;
+
+	return (z);
+}
+
+
+/**
+ * op_sub - sub two number.
+ *
+ * @a: the 1at number.
+ * @b: the 2nd number.
+ *
+ * Return: the calculation output.
+ *
+ */
+
+int op_sub(int a, int b)
+{
+	int z;
+
+	z = a - b;
+
+	return (z);
+}
+
+/**
+ * op_mul - mult two number.
+ *
+ * @a: the 1at number.
+ * @b: the 2nd number.
+ *
+ * Return: the calculation output.
+ *
+ */
+
+int op_mul(int a, int b)
+{
+	int z;
+
+	z = a * b;
+
+	return (z);
+}
+
+/**
+ * op_div - divide two number.
+ *
+ * @a: the 1at number.
+ * @b: the 2nd number.
+ *
+ * Return: the calculation output.
+ *
+ */
+
+int op_div(int a, int b)
+{
+	int z;
+
+	z = a / b;
+
+	return (z);
+}
+
+/**
+ * op_mod - a % b.
+ *
+ * @a: the 1at number.
+ * @b: the 2nd number.
+ *
+ * Return: the calculation output.
+ *
+ */
+
+int op_mod(int a, int b)
+{
+	int z;
+
+	z = a % b;
+
+	return (z);
+}
