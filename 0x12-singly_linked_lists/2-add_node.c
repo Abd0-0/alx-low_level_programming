a5bb70cb6f092e439b343192b01d46eb17f4007c
abd0-0 <abdaalrahman.mgdy2000@gmail.com>
2023-08-24 19:18:08 -0700

    Check

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -26,26 +26,3 @@ list_t *add_node(list_t **head, const char *str)
 
 	return (new);
 }
-
-/**
- * _strlen - a function that returns the length of a string.
- *
- * @s: a pointer to a string.
- *
- * Return: the length of the string.
- */
-
-int _strlen(const char *s)
-{
-	int i;
-
-	if (!s)
-		return (0);
-
-	for (i = 0; *s != '\0'; i++)
-	{
-		s++;
-	}
-
-	return (i);
-}
