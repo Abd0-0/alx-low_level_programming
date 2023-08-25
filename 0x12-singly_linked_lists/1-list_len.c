4231ad12a674280c3a1fbd2b1afd7090b61e6fb5
abd0-0 <abdaalrahman.mgdy2000@gmail.com>
2023-08-24 18:35:29 -0700

    Check

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,7 +2,7 @@
 
 
 /**
- * print_list - function that prints all the elements of a list_t list.
+ * list_len - function that counts all the elements of a list_t list.
  *
  * @h: the head of the list.
  *
