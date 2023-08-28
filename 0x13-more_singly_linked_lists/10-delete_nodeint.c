4245fcddd2267e673045b28437aaa2e7810b7166
abd0-0 <abdaalrahman.mgdy2000@gmail.com>
2023-08-28 13:03:04 -0700

    ?

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (!*head || !head)
 		return (-1);
 
-	if (index == i)
+	if (index <= i)
 	{
 		*head = cur->next;
 		free(cur);
@@ -40,5 +40,5 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(cur);
 		return (1);
 	}
-	return (1);
+	return (-1);
 }
