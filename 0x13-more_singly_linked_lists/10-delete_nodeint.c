4844ff0878f61f1c62b275b6f3a2db8ac3a8386c
abd0-0 <abdaalrahman.mgdy2000@gmail.com>
2023-08-28 13:09:32 -0700

    ?

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,7 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (index > i)
 	{
-		while (index != i)
+		while (index != i && ptr)
 		{
 			prev = cur;
 			cur =  cur->next;
