bb61969d1f72fc18cdbe863d28c0dc545aea2bc1
abd0-0 <abdaalrahman.mgdy2000@gmail.com>
2023-08-28 12:44:29 -0700

    ?

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new->next = NULL;
 	}
 
-	if (!head || !*head)
+	if (!head || !*head || !new)
 		return (NULL);
 
 	if (idx <= i && new)
