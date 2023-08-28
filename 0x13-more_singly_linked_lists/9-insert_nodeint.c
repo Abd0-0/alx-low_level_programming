a416ccee1239af6a8b9f8d2eb9f6a7b5491d89cf
abd0-0 <abdaalrahman.mgdy2000@gmail.com>
2023-08-28 12:26:36 -0700

    ?

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	while (idx - 1 > i && ptr)
+	while (idx > i && ptr)
 	{
 		ptr = ptr->next;
 		idx--;
