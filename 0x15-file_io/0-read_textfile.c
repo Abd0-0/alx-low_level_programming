5e4835d4d1dfccf659a96d6c7ba2a006a7274053
abd0-0 <abdaalrahman.mgdy2000@gmail.com>
2023-09-04 17:08:09 -0700

    Task0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the stdout.
+ *
+ * @filename: a pointer to file.
+ *
+ * @letters: the number of letters it should read and print.
+ *
+ * Return: the actual number of letters it could read and print.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char buffer[1024];
+	int fd;
+	ssize_t count;
+
+	if (!filename || !letters)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd < 0)
+		return (0);
+
+	count = read(fd, buffer, letters);
+	count = write(1, buffer, count);
+
+	if (count == -1)
+		return (0);
+
+	close(fd);
+
+	return (count);
+}
