11a13e4aedc1952248d16934fe1b205f4d03081d
abd0-0 <abdaalrahman.mgdy2000@gmail.com>
2023-09-04 18:17:40 -0700

    Task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,11 +25,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
 
 	if (fd == -1)
 		return (-1);
-	if (len > 0)
+	if (len)
 	{
 		count = write(fd, text_content, len);
 
