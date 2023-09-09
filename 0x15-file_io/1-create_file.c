40b24297c75b5ef7cb074c283630ff1d721d0e0a
abd0-0 <abdaalrahman.mgdy2000@gmail.com>
2023-09-09 10:13:13 -0700

    Check

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,7 +32,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (len == 0)
+	{
 		close(fd);
+		return (1);
+	}
+
 	else
 	{
 		count = write(fd, text_content, len);
