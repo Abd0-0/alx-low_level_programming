9202a4bd146b1bddb18c716f257a5a896f18d633
Abd0-0 <aabdalrahman.magdy2000@gmail.com>
2023-10-04 07:39:47 -0700

    TASKS(2, 3, 4, 5)

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,69 @@
+#include "main.h"
+
+/**
+ * argstostr - function that concatenates all the arguments of your program.
+ *
+ * @ac: argument count.
+ * @av: argument vector.
+ *
+ * Return: a pointer to a new string, or NULL if it fails.
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int size = 0, n = 0, i, j;
+	char *ptr;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			size++;
+		}
+		size++;
+	}
+
+	ptr =  malloc(size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			ptr[n] = av[i][j];
+			n++;
+		}
+
+		ptr[n] = '\n';
+		n++;
+	}
+	return (ptr);
+}
+
+/**
+ * _strlen - a function that returns the length of a string.
+ *
+ * @s: a pointer to a string.
+ *
+ * Return: the length of the string.
+ */
+
+int _strlen(const char *s)
+{
+	int i;
+
+	if (!s)
+		return (0);
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		s++;
+	}
+
+	return (i);
+}
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * alloc_grid - function that returns a pointer to a 2D_array of ints.
+ *
+ * @width: the width of the grid.
+ * @height: the height of hte grid.
+ *
+ * Return: a pointer to the 2D_array, or NULL.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, **ptr;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	ptr = (int **)malloc(height * sizeof(int *));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = (int *)malloc(width * sizeof(int));
+
+		if (ptr[i] == NULL)
+		{
+			for (j = 0; j <= i; j++)
+				free(ptr[j]);
+			free(ptr);
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+		{
+			ptr[i][j] = 0;
+		}
+	}
+
+	return (ptr);
+}
