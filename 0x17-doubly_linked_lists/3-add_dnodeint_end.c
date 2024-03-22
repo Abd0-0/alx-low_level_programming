b3b3ea9240be94fe14c3b93c3c3d808b0d54ec9f
Abd0_0 <abdalrahman.magdy2000@gmail.com>
2024-03-21 18:27:47 -0700

    task3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,38 +1,35 @@
 #include "lists.h"
 
-dlistint_t *make_node(int n);
-
 /**
- * add_dnodeint - adds a new node at the beginning
+ * add_dnodeint_end - adds a new node at the end
  * of a dlistint_t list
  *
  * @head: head of the list
  * @n: value of the element
  * Return: the address of the new element
  */
-
-
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
+	dlistint_t *current, *new;
 
 	new = make_node(n);
 	if (!new)
 		return (NULL);
 
-	if (*head == NULL)
-		*head = new;
-
-	else
+	if (!head || *head == NULL)
 	{
-		new->next = *head;
-		(*head)->prev = new;
 		*head = new;
+		return (new);
 	}
+
+	while (current->next)
+		current = current->next;
+
+	current->next = new;
+	new->prev = current;
 
 	return (new);
 }
-
 
 /**
  * make_node - makes a new dll node
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -32,4 +32,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 dlistint_t *make_node(int n);
 
+
 #endif
